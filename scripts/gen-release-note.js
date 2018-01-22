18d160514a0c83c1c109829e368232e3b35874be
ztc01 <ztc01@baidu.com>
2018-01-22 16:30:19 +0800

    feat: add gen-release-note command

---
+++ scripts/gen-release-note.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const pkg = require('../package.json');
+const version = process.argv[2] || process.env.VERSION || pkg.version;
+const cc = require('conventional-changelog');
+const file = `./RELEASE_NOTE${version ? `_${version}` : ``}.md`;
+const fileStream = require('fs').createWriteStream(file);
+
+cc({
+  preset: 'angular',
+  pkg: {
+    transform(pkg) {
+      pkg.version = `v${version}`;
+      return pkg;
+    }
+  }
+}).pipe(fileStream).on('close', () => {
+  console.log(`Generated release note at ${file}`);
+});
